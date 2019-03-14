3d704ebe5a2c4994cffa6717c598f38420a16095
Ebin Scaria <35758771+nibe130@users.noreply.github.com>
2019-03-14 16:12:14 -0400

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -1,3 +1,4 @@
+#Essential Libraries
 from os import path
 import requests
 from collections import Counter
@@ -11,17 +12,24 @@ warnings.filterwarnings("ignore")
 from wordcloud import WordCloud
 
 def wordCloud():
-    tags = open("C:/Users/ebin1/Desktop/tagss.txt", "r").read().split("\n")
-    dataset = pd.read_csv("C:/Users/ebin1/Desktop/Tags1.csv")
-    print(pd.Series(np.concatenate([x.split() for x in dataset.Tag])).value_counts())
-
+    #reads the words from a txt file 
+    tags = open("path/to/file", "r").read().split("\n")
+    #reads the words from a csv file(used to display the frequency of words)
+    dataset = pd.read_csv("path/to/file/file_name.csv")
+    #display frequency of each word
+    print(pd.Series(np.concatenate([word.split() for word in dataset.Tag])).value_counts())
+    
+    #dictionary with the frequecy of each word
     tagFreq = Counter()
     for word in tags:
         tagFreq[word] += 1
-    mask=np.array(Image.open(path.join("C:/Users/ebin1/Desktop/masks-wordclouds", "comment.png")))
-   #mask = np.array(Image.open(requests.get('http://www.clker.com/cliparts/O/i/x/Y/q/P/yellow-house-hi.png', stream=True).raw))
+    #cloud mask from local machine
+    mask=np.array(Image.open(path.join("path/to/mask/file", "mask_file_name.png")))
+    #cloud mask from an online resource
+    #mask = np.array(Image.open(requests.get('http://www.clker.com/cliparts/O/i/x/Y/q/P/yellow-house-hi.png', stream=True).raw))
     wordcloud = WordCloud(width=1000, height=900, background_color='white',relative_scaling=.8,mask=mask).generate_from_frequencies(tagFreq)
-    wordcloud.to_file("skills-cloud.png")
+    #save the word cloud as a png file
+    wordcloud.to_file("myfile.png")
     plt.imshow(wordcloud)
     plt.axis("off")
     plt.show()
